781d9f5f84d2d6389e99e63aa41c3d1a5431747a
lawka82 <Cola.1991@yandex.ru>
2026-07-09 11:49:56 +0400

    сделал класс оружия со свойствами и всякой хуйнёй

--- Weapon.cs
+++ Weapon.cs
@@ -1,6 +1,46 @@
 ﻿class Weapon
 {
-    public string Name;
-    public string Caliber;
-    public int Damage;
+    private string _name = "Unknown";
+    private string _caliber = "Unknown";
+    private int _damage = 0;
+    private static int _weaponCount = 0;
+    public Weapon(string name, string caliber, int damage)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            name = "Unknown";
+            Console.WriteLine("Неверный ввод");
+        }
+        else
+        {
+            _name = name;
+        }
+        if (string.IsNullOrWhiteSpace(caliber))
+        {
+            caliber = "Unknown";
+            Console.WriteLine("Неверный ввод");
+
+        }
+        else
+        {
+            _caliber = caliber;
+        }
+        if ( damage < 0)
+        {
+            damage = 0;
+            Console.WriteLine("Урон меньше нуля");
+        }
+        else
+        {
+            _damage = damage;
+        }
+        _weaponCount++;
+    }
+    public void PrintWeaponInfo()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Caliber: {_caliber}");
+        Console.WriteLine($"Damage: {_damage}");
+        Console.WriteLine($"Weapon count: {_weaponCount}");
+    }
 }
