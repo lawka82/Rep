0e630a30bd298e58f5c6b51dce86c7fd8fe182c2
lawka82 <Cola.1991@yandex.ru>
2026-07-09 15:09:56 +0400

    предвнедрение общего класса

---
+++ GameEntity.cs
@@ -0,0 +1,23 @@
+﻿public class GameEntity
+{
+    public string _name = "Unknown";
+    private static int _entityCount = 0;
+    public static int EntityCount => _entityCount;
+        public string GeName
+    {
+        get => _name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value)) 
+            {
+                _name = "Unknown";
+                    }
+            else { _name = value; }
+        }
+
+    }
+    public string UnqName(string name)
+    {
+        GeName = name;
+    }
+}
